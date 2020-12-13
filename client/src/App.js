37ca2fdcfc161f7f365fdfd612ee369b70920498
Yaşar Çakır <yasar@Yasar-MacBook-Pro.local>
2020-12-13 20:48:36 +0300

    react state

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+
+import ContactState from './context/contact/ContactState'
+
+function App() {
+	return (
+		<ContactState>
+			<Router>
+				<Navbar />
+				<div className="container">
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/about" component={About} />
+					</Switch>
+				</div>
+			</Router>
+		</ContactState>
+	)
+}
+
+export default App
--- server.js
+++ server.js
@@ -4,18 +4,13 @@ const auth = require('./routes/auth')
 const contacts = require('./routes/contacts')
 const conn = require('./config/db')
 
-
-
-
 // COnnect Database
 conn()
 
-
 const app = express()
 
-
-app.use(express.json({extended: false}))
-app.use(express.urlencoded({extended: false}))
+app.use(express.json({ extended: false }))
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/users', users)
 app.use('/api/auth', auth)
@@ -24,5 +19,5 @@ app.use('/api/contacts', contacts)
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-    console.log(`server started on port ${port}`)
+	console.log(`server started on port ${port}`)
 })
